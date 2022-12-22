6413131c7a40141b485162c7e9a307e2f2045f1f
Olayeni Gbenga (George) <biadedigital1@gmail.com>
2022-12-22 13:48:06 +0000

    render all page on error

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-  if(req.url === '/'){
-   res.end('Home Page')
+  if (req.url === '/') {
+    res.end('Home Page')
   }
-  if(req.url === '/about'){
-   res.end('About Page')
+  if (req.url === '/about') {
+    res.end('About Page')
   }
-  if(req.url === '/contact'){
-   res.end('Contact Page')
+  if (req.url === '/contact') {
+    res.end('Contact Page')
   }
   res.end(
     `<p>Error Page</p>
     <a href="/">Go back to HomePage</a>
+    <a href="/about">Go back to AboutPage</a>
+    <a href="/contact">Go back to ContactPage</a>
     `
   )
 })
